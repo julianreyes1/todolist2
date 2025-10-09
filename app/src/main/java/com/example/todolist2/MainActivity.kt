3d4c3d7df740cb8dbcb137d992fe5b60643956d4
julianreyes1 <julian.messier45@gmail.com>
2025-10-08 19:04:10 -0600

    App

---
+++ app/src/main/java/com/example/todolist2/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.todolist2
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btn: MaterialButton = findViewById(R.id.btn_login)
+        btn.setOnClickListener {
+
+            val intent : Intent = Intent(this, Pantalla2::class.java)
+            startActivity(intent)
+
+        }
+    }
+}
